b48777e5bd3686b4b8852bb18e76aeebd5f1b7da
AlvesArthur1983 <145377096+AlvesArthur1983@users.noreply.github.com>
2024-01-02 10:04:13 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+var r = document.getElementById("redireciona");
+r.addEventListener("click", function(){
+    window.location.href = "https://designweb1v.github.io/designweb1v.io/Cadastro"
+})
+
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('email');
+    var senha = document.getElementById('Senha');
+    var emailError = document.getElementById('emailError');
+    var senhaError = document.getElementById('senhaError');
+
+    if (!email.value.endsWith('@escolar.ifrn.edu.br')) {
+        emailError.textContent = 'O email deve terminar com @escolar.ifrn.edu.br';
+        email.style.border = '1px solid red';
+        
+    } else {
+        emailError.textContent = '';
+        email.style.border = '1px solid blue';
+    }
+
+    if (senha.value.length < 8) {
+        senhaError.textContent = 'A senha deve ter pelo menos 8 caracteres';
+        senha.style.border = '1px solid red';
+    } else {
+        senhaError.textContent = '';
+        senha.style.border = '1px solid blue';
+    }
+
+    if (emailError.textContent == '' && senhaError.textContent == '') {
+        event.currentTarget.submit();
+    }
+});
+
+
